0e78490ee28e6790cfe2a310e0ae313e02da0ee0
puripuri2100 <puripuri2100@gmail.com>
2025-02-25 20:56:02 +0900

    見た目をアップデートし、関連法律文書を表示するようにした

--- src/search.js
+++ src/search.js
@@ -122,16 +122,82 @@ function displayResults(data) {
   const resultsDiv = document.getElementById("results");
   resultsDiv.innerHTML = "";
   data.sort((a, b) => a[0] - b[0]);
-  data.forEach((item) => {
+  data.forEach((item, index) => {
       const contents = item.contents;
       const first_data = contents[0];
       const first_document = first_data.document;
       const first_data_url = legal_document_url(first_document);
+      let item_div = document.createElement("div");
+      item_div.setAttribute("class", "legal_document");
+      item_div.id = first_data.document.id;
       let link = document.createElement("a");
       link.href = first_data_url;
       link.textContent = first_document.name;
       link.target = "_blank";
-      resultsDiv.appendChild(link);
-      resultsDiv.appendChild(document.createElement("br"));
+      item_div.appendChild(link);
+      // 参照している法律文書を取得して表示
+      let parents = document.createElement("details");
+      let parents_summary = document.createElement("summary");
+      parents_summary.textContent = "この文書が参照している法律文書";
+      parents.appendChild(parents_summary);
+      let parents_items = document.createElement("ul");
+      item.parents.forEach((id) => {
+        const link_contents = data.find((item_info) => item_info.contents[0].document.id === id);
+        if (link_contents && link_contents.contents.length != 0) {
+          const link_content = link_contents.contents[0];
+          let link_name = "";
+          if (link_content.document.type == "Law") {
+            link_name = link_content.document.name;
+          } else if (link.content.document.type == "Precedent") {
+            link_name = link_content.document.id;
+          }
+          let parent_item_link = document.createElement("a");
+          parent_item_link.href = `#${id}`;
+          parent_item_link.textContent = link_name;
+          let parent_item = document.createElement("li");
+          parent_item.appendChild(parent_item_link);
+          parents_items.appendChild(parent_item);
+        }
+      });
+      parents.appendChild(parents_items);
+      if (item.parents.length != 0) {
+        item_div.appendChild(parents);
+      }
+
+      // 参照されている法律文書を表示する
+      let children = document.createElement("details");
+      let children_summary = document.createElement("summary");
+      children_summary.textContent = "この文書が参照されている法律文書";
+      children.appendChild(children_summary);
+      let children_items = document.createElement("ul");
+      item.children.forEach((id) => {
+        const link_contents = data.find((item_info) => item_info.contents[0].document.id === id);
+        if (link_contents && link_contents.contents.length != 0) {
+          const link_content = link_contents.contents[0];
+          let link_name = "";
+          if (link_content.document.type == "Law") {
+            link_name = link_content.document.name;
+          } else if (link.content.document.type == "Precedent") {
+            link_name = link_content.document.id;
+          }
+          let children_item_link = document.createElement("a");
+          children_item_link.href = `#${id}`;
+          children_item_link.textContent = link_name;
+          let children_item = document.createElement("li");
+          children_item.appendChild(children_item_link);
+          children_items.appendChild(children_item);
+        }
+      });
+      children.appendChild(children_items);
+      if (item.children.length != 0) {
+        item_div.appendChild(children);
+      }
+
+      if (index != 0) {
+        const line = document.createElement("hr");
+        resultsDiv.appendChild(line);
+      }
+
+      resultsDiv.appendChild(item_div);
   });
 }
